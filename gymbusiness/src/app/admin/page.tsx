0afb58e6ed415fdf61103c8309a0e38ba7ccdd23
Anthony Solis <109778441+Anthony6887@users.noreply.github.com>
2025-08-12 23:32:35 -0500

    Login y Pagina admin provicional y estructurada

---
+++ gymbusiness/src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4">Bienvenido al Panel Administrativo</h2>
+            <p>Aquí puedes gestionar todo el contenido de tu sistema.</p>
+        </div>
+    );
+}
