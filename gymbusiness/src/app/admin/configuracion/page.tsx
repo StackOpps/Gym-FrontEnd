cd3ab4ad167b65e216fbecc9e2f63397d25e3b13
Anthony Solis <109778441+Anthony6887@users.noreply.github.com>
2025-08-13 23:53:13 -0500

    Estructura del administrador, pantallas resposivas y adaptado para modo oscuro y claro

--- gymbusiness/src/app/admin/configuracion/page.tsx
+++ gymbusiness/src/app/admin/configuracion/page.tsx
@@ -1,13 +1,10 @@
-export default function AdminPage() {
+export default function ConfiguracionPage() {
     return (
         <div className="w-full min-h-screen px-4 sm:px-6 lg:px-8 py-6">
             <div className="max-w-5xl mx-auto">
                 <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-4">
-                    Bienvenido al Panel Administrativo
+                    Configuración
                 </h2>
-                <p className="text-sm sm:text-base text-gray-700 dark:text-gray-300">
-                    Aquí puedes gestionar todo el contenido de tu sistema.
-                </p>
             </div>
         </div>
     );
--- gymbusiness/src/app/layout.tsx
+++ gymbusiness/src/app/layout.tsx
@@ -1,30 +1,35 @@
 "use client";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
 import { ThemeProvider } from './context/ThemeContext';
 import './globals.css';
 import WhatsappButton from './components/WhatsappButton';
 import { usePathname } from 'next/navigation';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const noHeaderFooterRoutes = ['/login'];
+  const isAdminRoute = pathname.startsWith('/admin');
 
-  const noHeaderFooterRoutes = ['/login', '/admin'];
-
-  const showHeaderFooter = !noHeaderFooterRoutes.some(route => pathname.startsWith(route));
-
-  const paddingTopClass = showHeaderFooter ? 'pt-16 lg:pt-20' : 'pt-0';
+  const showHeaderFooter =
+    !noHeaderFooterRoutes.some(route => pathname.startsWith(route)) && !isAdminRoute;
 
   return (
     <html lang="es">
       <body className="antialiased bg-bg text-fg transition-colors duration-300 min-h-screen flex flex-col">
         <ThemeProvider>
+          {/* Header solo en rutas públicas */}
           {showHeaderFooter && <Header />}
-          <main className={`flex-grow ${paddingTopClass}`}>
+
+          {/* Contenido */}
+          <main className={`flex-grow ${showHeaderFooter ? 'pt-16 lg:pt-20' : 'pt-0'}`}>
             {children}
           </main>
+
+          {/* Footer solo en rutas públicas */}
           {showHeaderFooter && <Footer />}
+
           <WhatsappButton />
         </ThemeProvider>
       </body>
