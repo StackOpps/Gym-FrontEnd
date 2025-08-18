8bc1f0963e5f9097cdfb8035e8329e9e7504b51b
CriSant20 <92830667+CriSant20@users.noreply.github.com>
2025-08-17 19:28:56 -0500

    se completan las carpetas para sidebar

---
+++ gymbusiness/src/app/admin/horarios/page.tsx
@@ -0,0 +1,11 @@
+export default function SchedulesPage() {
+    return (
+        <div className="w-full min-h-screen px-4 sm:px-6 lg:px-8 py-6">
+            <div className="max-w-5xl mx-auto">
+                <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-4">
+                    Horarios
+                </h2>
+            </div>
+        </div>
+    );
+}
